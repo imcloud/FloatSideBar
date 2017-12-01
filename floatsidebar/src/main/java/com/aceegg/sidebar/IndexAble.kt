428abd5a35a563d958896bd4efad365d227858ac
imcloud <240081368@qq.com>
2017-12-01 15:16:07 +0800

    add comment

--- floatsidebar/src/main/java/com/aceegg/sidebar/IndexAble.kt
+++ floatsidebar/src/main/java/com/aceegg/sidebar/IndexAble.kt
@@ -1,8 +1,12 @@
 package com.aceegg.sidebar
 
 /**
+ * 可传入实现此接口的对象数组做索引
  * Created by jinwenxiu on 2017/12/1.
  */
 interface IndexAble {
+    /**
+     * 定义你的对象对应的索引
+     */
     fun indexOfThisObject(): String
 }
